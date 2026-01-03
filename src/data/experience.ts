32448090a57d99aac62f2ea44a8464fde05222bc
Pranav Sharma <mechanicm56@gmail.com>
2026-01-03 12:05:15 +0530

    updated

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -1,30 +1,30 @@
 import { type ExperienceShowcaseListItemProps } from "@/components/experience/experience-showcase-list-item";
 
 export const EXPERIENCE: ExperienceShowcaseListItemProps[] = [
-  // {
-  //   title: "Full Stack Engineer",
-  //   organisation: {
-  //     name: "Intellizon Systems, Hyderabad",
-  //     href: "https://masterinfotech.com/",
-  //   },
-  //   date: "Nov, 2021 - Nov, 2022",
-  //   location: "Remote",
-  //   description:
-  //     "Worked on a team responsible for developing new features and updating old codebases to latest technologies. Learned alot about web development and CI/CD development cycle.",
-  // },
   {
-    title: "AI / ML Intern",
+    title: "Full Stack Engineer",
     organisation: {
-      name: "Labmentix, Bengaluru, Karnataka",
-      href: "https://labmentix.in/",
+      name: "Intellizon Systems, Hyderabad",
+      href: "",
     },
-    date: "Oct, 2025 - Present",
+    date: "Nov, 2021 - Nov, 2022",
     location: "Remote",
-    description: [
-      "Work with Data extraction from csv, json, sql database.",
-      "Visualize Data using seaborn library Computer Vision, Augmentation, YOLOv8",
-      "ML models from sklearn with MLflow integration",
-      "Dashboard & Reports deploy with Streamlit App",
-    ],
+    description:
+      "Worked on a team responsible for developing new features and updating old codebases to latest technologies. Learned alot about web development and CI/CD development cycle.",
   },
+  // {
+  //   title: "AI / ML Intern",
+  //   organisation: {
+  //     name: "Labmentix, Bengaluru, Karnataka",
+  //     href: "https://labmentix.in/",
+  //   },
+  //   date: "Oct, 2025 - Present",
+  //   location: "Remote",
+  //   description: [
+  //     "Work with Data extraction from csv, json, sql database.",
+  //     "Visualize Data using seaborn library Computer Vision, Augmentation, YOLOv8",
+  //     "ML models from sklearn with MLflow integration",
+  //     "Dashboard & Reports deploy with Streamlit App",
+  //   ],
+  // },
 ];
